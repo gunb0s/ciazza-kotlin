9c7b0a6b10d68244243a8f4cc005fea7c31c5bd1
cain <gunbos1031@gmail.com>
2024-03-01 03:11:39 +0900

    feat: test env file

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
