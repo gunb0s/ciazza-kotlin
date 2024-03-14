a0acac4471ca812031dc36e48634ebc673a3b629
cain <gunbos1031@gmail.com>
2024-03-14 22:30:41 +0900

    github oauth2 request

--- src/main/kotlin/gunb0s/toy/ciazzakotlin/auth/controller/AuthController.kt
+++ src/main/kotlin/gunb0s/toy/ciazzakotlin/auth/controller/AuthController.kt
@@ -1,25 +1,53 @@
 package gunb0s.toy.ciazzakotlin.auth.controller
 
 import gunb0s.toy.ciazzakotlin.auth.controller.dto.LoginRequestDto
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.http.ResponseEntity
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.toEntity
+
+data class GithubAccessTokenRequest(
+    val client_id: String,
+    val client_secret: String,
+    val code: String,
+)
+
+data class GithubAccessTokenResponse(
+    val access_token: String,
+    val token_type: String,
+    val scope: String,
+)
 
 @RestController
 class AuthController(
-    val authenticationManager: AuthenticationManager,
+    @Value("\${GITHUB_CLIENT_ID}")
+    val clientId: String,
+    @Value("\${GITHUB_CLIENT_SECRET}")
+    val clientSecret: String,
 ) {
-    @PostMapping("/auth/login")
-    fun login(@RequestBody loginRequest: LoginRequestDto): ResponseEntity<Void> {
-        val authenticateRequest = UsernamePasswordAuthenticationToken.unauthenticated(
-            loginRequest.username,
-            loginRequest.password
-        )
-        val authenticationResponse = authenticationManager.authenticate(authenticateRequest)
+    @CrossOrigin
+    @PostMapping("/auth/github")
+    fun login(@RequestBody loginRequest: LoginRequestDto): ResponseEntity<GithubAccessTokenResponse> {
+        val defaultClient = RestClient.create()
 
-        return ResponseEntity.ok().build()
+        val requestUrl = "https://github.com/login/oauth/access_token"
+
+        val githubAccessTokenRequest = GithubAccessTokenRequest(
+            clientId,
+            clientSecret,
+            loginRequest.code
+        )
+
+        return defaultClient.post()
+            .uri(requestUrl)
+            .accept(APPLICATION_JSON)
+            .body(githubAccessTokenRequest)
+            .retrieve()
+            .toEntity<GithubAccessTokenResponse>()
     }
 }
--- src/main/kotlin/gunb0s/toy/ciazzakotlin/auth/controller/dto/LoginRequestDto.kt
+++ src/main/kotlin/gunb0s/toy/ciazzakotlin/auth/controller/dto/LoginRequestDto.kt
@@ -1,6 +1,5 @@
 package gunb0s.toy.ciazzakotlin.auth.controller.dto
 
 data class LoginRequestDto(
-    val username: String,
-    val password: String,
+    val code: String,
 )
